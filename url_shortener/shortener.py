f243925cbda8e2f81c758bf3fedcba70ea54de9e
Sean Hart <shart@shart.io>
2019-07-30 16:21:22 -0700

    wip

--- url_shortener/shortener.py
+++ url_shortener/shortener.py
@@ -1,2 +1,46 @@
+import hashlib
+
 def working():
     return True
+
+base_domain = 'https://go.to/'
+urls = []
+
+class Url(object):
+    """
+    An Object to store data for urls
+        Attributes:
+            url: String
+            short_url: String
+            ttl: Integer (seconds until expire)
+            create_time: DateTime
+    """
+    
+    def __init__(self, url=None, short_url=None, ttl=60):
+        self.url = url
+        self.short_url = short_url
+        self.ttl = ttl
+
+
+
+    def __repr__(self):
+        return f"ShortURL: {self.short_url}, FullURL: {self.url}, TTL: {self.ttl}"
+
+def get_url(short_url):
+    """
+    Fetch the URL from the storage system
+    """
+    url_object = [x for x in urls if x.short_url == short_url].pop()
+    return url_object
+    
+def set_url(long_url):
+    # Let's grab the first 4 characters of an mmd5 hash as an identifier
+    identifier = hashlib.md5(long_url.encode("utf-8")).hexdigest()[:4]
+    short_url = f'{base_domain}{identifier}'
+    url_object = Url(long_url, short_url)
+    urls.append(url_object)
+    return url_object
+
+def garbage_collection():
+    pass
+    
--- url_shortener/shortener_test.py
+++ url_shortener/shortener_test.py
@@ -1,8 +1,29 @@
 import unittest
 
-import shortener
+from shortener import(
+    get_url,
+    set_url,
+    working,
+    Url,
+) 
 
 class ShortenerTest(unittest.TestCase):
 
     def test_working(self):
-        self.assertEqual(shortener.working(), True)
+        self.assertEqual(working(), True)
+
+    def test_url_class(self):
+        url = Url('http://google.com', 'http://go.to/AAAA')
+        self.assertEqual(url.url, 'http://google.com')
+        self.assertEqual(url.short_url, "http://go.to/AAAA")
+
+    def test_set_url(self):
+        url = set_url('https://google.com')
+        self.assertEqual(url.short_url, 'https://go.to/9999' )
+
+    def test_get_url(self):
+        set_url('https://google.com')
+        fetched_url = get_url('https://go.to/9999')
+        self.assertEqual(fetched_url.url, 'https://google.com')
+        
+        
